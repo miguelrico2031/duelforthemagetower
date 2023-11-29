34006586a5938982d0bf6e83a17df9b0011ab456
eze <69222000+quiellll@users.noreply.github.com>
2023-11-29 18:28:07 +0100

    Botones animados
    Los botones tienen animación al hacer clic.
    
    Hay una advertencia cuando se continúa el juego pulsando el botón en lugar de darle a escape. No sé exactamente por qué, ya que los dos eventos lanzan el mismo código...
    
    Ahora les pondré sonido

--- JS/pause.js
+++ JS/pause.js
@@ -20,9 +20,9 @@ class Pause extends Phaser.Scene
 
     preload()
     {
-        this.load.image("pause_screen", "../Assets/UI/Pause Menus/Pause Base.png");
-        this.load.image("continuar", "../Assets/UI/Pause Menus/PAUSA_DUMMY.png");
-        this.load.image("salir", "../Assets/UI/Pause Menus/CONTINUAR_DUMMY.png");
+        this.load.image("pause_screen", "../Assets/UI/Pause Menus/Pause no buttons.png");
+        this.load.spritesheet("continuar", "../Assets/UI/Pause Menus/Continue_spritesheet.png", { frameWidth: 167, frameHeight: 106 });
+        this.load.spritesheet("salir", "../Assets/UI/Pause Menus/Exit_spritesheet.png", { frameWidth: 167, frameHeight: 106 });
     }
 
     create()
@@ -31,10 +31,24 @@ class Pause extends Phaser.Scene
 
         this.add.image(0, 0, "pause_screen").setOrigin(0, 0);
 
+        this.anims.create
+        ({
+            key: "continue_press",
+            frames: this.anims.generateFrameNumbers("continuar", { start: 0, end: 1 }),
+            frameRate: 12,
+        });
+
+        this.anims.create
+        ({
+            key: "exit_press",
+            frames: this.anims.generateFrameNumbers("salir", { start: 0, end: 1 }),
+            frameRate: 12,
+        });
+
         // La forma de poner los botones es lo mas terrorifico feo e ineficiente que he hecho en mi vida dios mio
         // pero tampoco hay otra pq usar el setorigin con estas da errores y por lo q sea usar solo el viewport no las centra
-        this.buttonContinue = this.add.image(viewport.width/2, viewport.height/2 - 10, "continuar");
-        this.buttonExit = this.add.image(viewport.width/2, 120 + viewport.height/2, "salir");
+        this.buttonContinue = this.add.sprite(viewport.width/2, viewport.height/2 - 10, "continuar");
+        this.buttonExit = this.add.sprite(viewport.width/2, 120 + viewport.height/2, "salir");
         
         this.buttonContinue.setInteractive();
         this.buttonExit.setInteractive();
@@ -45,7 +59,6 @@ class Pause extends Phaser.Scene
     
     update()
     {
-        console.log("paused: " + this.gameIsPaused)
         this.processInput();
     }
 
@@ -61,18 +74,13 @@ class Pause extends Phaser.Scene
 
         // Eventos de ratón
         this.buttonContinue.on('pointerdown', () => { this.resumeGame(); });
-        this.buttonExit.on('pointerdown', () => { console.log('Salir del Juego'); });
+        this.buttonExit.on('pointerdown', () => { this.exitGame(); });
+
+        // creo q es un poco chapucero pero es la que hay no encuentro nada mas en la referencia
+        this.buttonContinue.on('pointerover', () => { this.buttonContinue.setFrame(0); });
+        this.buttonExit.on('pointerup', () => { this.buttonExit.setFrame(0); });
     }
     
-    resumeGame()
-    {
-        this.pauseKeyIsPressed = false;
-        // aqui la gracia es hacer que esta escena de pausa se oculte
-        //this.scene.sendToBack("PauseScene"); // la oculta pero luego no puedo volver a poner el juego en pausa
-        this.scene.sleep("PauseScene");
-        this.scene.resume("GameplayScene"); // continua la ejecucion del juego
-    }
-
     checkPauseToggled() 
     {
         if (this.pauseKey.isDown && !this.pauseKeyIsPressed) {
@@ -85,5 +93,23 @@ class Pause extends Phaser.Scene
             this.pauseKeyIsPressed = false;
             this.gameIsPaused = false;
         }
+    }
+
+    resumeGame()
+    {
+        this.pauseKeyIsPressed = false;
+        this.add.sprite(this.buttonContinue.x, this.buttonContinue.y).play("continue_press");
+        this.buttonExit.setFrame(0);
+        // aqui la gracia es hacer que esta escena de pausa se oculte
+        //this.scene.sendToBack("PauseScene"); // la oculta pero luego no puedo volver a poner el juego en pausa
+        this.scene.sleep("PauseScene");
+        this.scene.resume("GameplayScene"); // continua la ejecucion del juego
+    }
+
+    exitGame()
+    {
+        console.log("Salir del juego");
+        //this.add.sprite(this.buttonExit.x, this.buttonExit.y).play("exit_press"); // va feo
+        this.buttonExit.setFrame(1);
     }
 }

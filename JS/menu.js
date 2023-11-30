8b5d094e193f8b381bd70c5cfaf6d749a9ce4436
Gloria <146942219+gloriabllorente@users.noreply.github.com>
2023-11-30 12:09:09 +0100

    guardar cambios
    que me cambio de ordenador jiji

--- JS/menu.js
+++ JS/menu.js
@@ -7,12 +7,18 @@ class Menu extends Phaser.Scene
     menuKeyIsPressed;
 
     //privadas
+
+    buttonPressed;
+
     menuScreen;
     menuKey;
 
     buttonPlay;
     buttonHelp;
     buttonCredits;
+
+    audioClick;
+    audioClack;
 
     //Metodos publicos
     constructor(scene) 
@@ -26,17 +32,28 @@ class Menu extends Phaser.Scene
         this.load.image("play", "../Assets/UI/Menu Principal/play_dummy.png");
         this.load.image("help", "../Assets/UI/Menu Principal/help_dummy.png");
         this.load.image("credits", "../Assets/UI/Menu Principal/credits_dummy.png");
+        this.load.audio("click", "../Assets/UI/Sounds/Minimalist4.wav");
+        this.load.audio("clack", "../Assets/UI/Sounds/Minimalist7.wav");
     }
 
     create()
     {
+        this.buttonPressed = false;
+
         this.mainMenu = true;
 
         this.add.image(0, 0, "menu").setOrigin(0, 0);
 
+        this._audioClick = this.sound.add("click");
+        this._audioClack = this.sound.add("clack");
+
         //mitad izquierda
         this.buttonPlay = this.add.image(game.config.width / 2.833, game.config.height / 2, "play");
         this.buttonPlay.setInteractive().on('pointerdown', this.startGame, this);
+
+        this.buttonPlay = this.initPlayButton();
+
+
 
         //mitad derecha, arriba
         this.buttonHelp = this.add.image((game.config.width / 4.425) * 3, game.config.height / 3.6, "help");

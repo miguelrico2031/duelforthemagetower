0eccff0e6997946ffd1eb704ffa323702a4adefa
Tokpary <TheFrikGameToli@gmail.com>
2023-12-17 05:33:41 +0100

    Estadisticas final de partida

---
+++ duelforthemagetower/bin/src/main/resources/static/JS/gameover.js
@@ -0,0 +1,115 @@
+class Gameover extends Phaser.Scene{
+
+
+    //private 
+    _buttonPressed;
+    _winnerPlayer;
+
+    _buttonMenu;
+    _buttonPlayAgain;
+
+    _audioClick;
+    _audioClack;
+
+    constructor(){
+        super("GameoverScene");
+    }
+
+    preload(){
+        this.load.image("p1_win", "../Assets/UI/Screens/GameOver/P1Win.png");
+        this.load.image("p2_win", "../Assets/UI/Screens/GameOver/P2Win.png");
+
+        this.load.spritesheet("btn_menu", "../Assets/UI/Screens/GameOver/MenuButton.png", { frameWidth: 167, frameHeight: 106 });
+        this.load.spritesheet("btn_replay", "../Assets/UI/Screens/GameOver/ReplayButton.png", { frameWidth: 167, frameHeight: 106 });
+        this.load.audio("click", "../Assets/UI/Sounds/Minimalist4.wav");
+        this.load.audio("clack", "../Assets/UI/Sounds/Minimalist7.wav");
+    }
+
+    init(data){
+        this._winnerPlayer = data.winner;
+    }
+
+    create(){
+        this._buttonPressed = false;
+
+        if(this._winnerPlayer === 1){
+            this.add.image(0, 0, "p1_win").setOrigin(0,0);
+        } else {
+            this.add.image(0, 0, "p2_win").setOrigin(0,0);
+        }
+        
+
+        this._audioClick = this.sound.add("click");
+        this._audioClack = this.sound.add("clack");
+        
+        this._buttonMenu = this.initMenuButton();
+
+        this._buttonPlayAgain = this.initPlayAgainButton();
+    }
+
+    
+    enterButtonClickState(button) 
+    {
+        this._audioClick.play(); 
+        button.setFrame(1);
+        this._buttonPressed = true;
+    }
+
+    enterButtonRestState(button)
+    {
+        if(this._buttonPressed) this._audioClack.play();
+        button.setFrame(0);
+        this._buttonPressed = false;
+    }
+
+    initMenuButton()
+    {
+        let button = this.add.sprite(viewport.width/2, 120 + viewport.height/2, "btn_menu")
+            .setInteractive({ useHandCursor: true })
+            .on('pointerdown', () => { this.enterButtonClickState(this._buttonMenu) })
+            .on('pointerup', () => 
+            { 
+                this.enterButtonRestState(this._buttonMenu);
+                this.exitMenu(); 
+            })
+            .on('pointerout', () => this.enterButtonRestState(this._buttonMenu) 
+        );
+
+        return button;
+    }
+
+    initPlayAgainButton()
+    {
+        let button = this.add.sprite(viewport.width/2, viewport.height/2 - 10, "btn_replay")
+            .setInteractive({ useHandCursor: true })
+            .on('pointerdown', () => { this.enterButtonClickState(this._buttonPlayAgain) })
+            .on('pointerup', () => 
+            { 
+                this.enterButtonRestState(this._buttonPlayAgain);
+                this.restartGame(); 
+            }) 
+            .on('pointerout', () => this.enterButtonRestState(this._buttonPlayAgain)
+        );
+
+        return button;
+    }
+
+    restartGame()
+    {
+        this.game.sound.stopAll();
+        this.scene.restart("GameplayScene"); // reinicia la escena del juego
+        this.scene.start("GameplayScene"); 
+    }
+
+    exitMenu()
+    {   
+        console.log("Salir al menú");
+        this.game.sound.stopAll();
+        this.scene.launch("MenuScene", { isPlaying: false });
+        this.scene.stop("GameplayScene");
+        this.scene.sleep("GameoverScene");
+        
+    }
+
+
+}
--- duelforthemagetower/src/main/java/com/dftmt/UserStats.java
+++ duelforthemagetower/src/main/java/com/dftmt/UserStats.java
@@ -5,42 +5,88 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class UserStats
 {
 	private final String username;
-	private int stat1;
-	private int stat2;
+	private int hitsTaken;
+	private int hitsGiven;
+	private int hitsDeflected;
+	private int wins;
+	private int losses;
 	
 	public UserStats(String username)
 	{
 		this.username = username;
-		stat1 = 0;
-		stat2 = 0;
+		this.hitsTaken = 0;
+		this.hitsGiven = 0;
+		this.hitsDeflected = 0;
+		this.wins = 0;
+		this.losses = 0;
 	}
 	
-	public UserStats(@JsonProperty("username")String username, @JsonProperty("stat1")int stat1, @JsonProperty("stat2")int stat2)
+	public UserStats(@JsonProperty("username")String username, @JsonProperty("hitsGiven")int hitsGiven, @JsonProperty("hitsTaken")int hitsTaken, @JsonProperty("hitsDeflected")int hitsDeflected, @JsonProperty("wins")int wins, @JsonProperty("losses")int losses)
 	{
 		this.username = username;
-		this.stat1 = stat1;
-		this.stat2 = stat2;
+		this.hitsGiven = hitsGiven;
+		this.hitsTaken = hitsTaken;
+		this.hitsDeflected = hitsDeflected;
+		this.wins = wins;
+		this.losses = losses;
 	}
 
 	
 	public void copyStats(UserStats stat)
 	{
-		stat1 = stat.getStat1();
-		stat2 = stat.getStat2();
+		hitsGiven = stat.getHitsGiven();
+		hitsTaken = stat.getHitsTaken();
+		hitsDeflected = stat.getHitsDeflected();
+		wins = stat.getWins();
+		losses = stat.getLosses();
 	}
 	
 	public String getUsername() { return username; }
-	public int getStat1() { return stat1; }
-	public int getStat2() { return stat2; }
-	public void setStat1(int s) { stat1 = s; }
-	public void setStat2(int s) { stat2 = s; }
+	public int getHitsGiven() { return hitsGiven; }
+	public int getHitsTaken() { return hitsTaken; }
+	public int getHitsDeflected() { return hitsDeflected; }
+	public int getWins() { return wins; }
+	public int getLosses() { return losses; }
 	
-	public void increaseStat1(int amount)
-	{
-		stat1 += amount;
+	public void setHitsTaken(int hitsTaken) {
+		this.hitsTaken = hitsTaken;
 	}
-	public void increaseStat2(int amount)
+
+	public void setHitsGiven(int hitsGiven) {
+		this.hitsGiven = hitsGiven;
+	}
+
+	public void setHitsDeflected(int hitsDeflected) {
+		this.hitsDeflected = hitsDeflected;
+	}
+
+	public void setWins(int wins) {
+		this.wins = wins;
+	}
+
+	public void setLosses(int losses) {
+		this.losses = losses;
+	}
+
+	public void increaseHitsGiven(int amount)
 	{
-		stat2 += amount;
+		hitsGiven += amount;
+	}
+	public void increaseHitsTaken(int amount)
+	{
+		hitsTaken += amount;
+	}
+	
+	public void increaseHitsDeflected(int amount)
+	{
+		hitsDeflected += amount;
+	}
+	public void increaseWins(int amount)
+	{
+		wins += amount;
+	}
+	public void increaseLosses(int amount)
+	{
+		losses += amount;
 	}
 }
